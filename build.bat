dfe542c85b20e509744752ae0072f39cdf0064d6
Leandro <leandroserrano777@gmail.com>
2026-05-04 20:51:01 -0300

    fix: build.bat and add valid Windows icon
    - Converted icon.png to valid multi-size ICO (16,32,64,128,256px)
    - Fixed build.bat to use windres with cpp preprocessor
    - Added MinGW bin to PATH in build.bat
    - Icon now embeds correctly in FastEditor.exe

--- build.bat
+++ build.bat
@@ -1,13 +1,25 @@
 @echo off
+setlocal enabledelayedexpansion
+
 echo Compilando FastEditor...
+
+set PATH=%PATH%;C:\Program Files\CodeBlocks\MinGW\bin
+
 echo [1/3] Compilando recurso de icono...
-"C:\Program Files\CodeBlocks\MinGW\bin\windres.exe" resource.rc -O coff -o resource.res
+windres --preprocessor=cpp resource.rc -O coff -o resource.res
+if %ERRORLEVEL% NEQ 0 (
+    echo ERROR: No se pudo compilar el recurso (icono)
+    pause
+    exit /b 1
+)
+
 echo [2/3] Compilando codigo...
 g++ -std=c++14 -Iimgui main.cpp imgui/imgui.cpp imgui/imgui_demo.cpp imgui/imgui_draw.cpp imgui/imgui_tables.cpp imgui/imgui_widgets.cpp imgui/imgui_impl_win32.cpp imgui/imgui_impl_opengl3.cpp resource.res -lopengl32 -lgdi32 -ldwmapi -lcomdlg32 -mwindows -o FastEditor.exe
 if %ERRORLEVEL% NEQ 0 (
     echo ERROR DE COMPILACION
     pause
-) else (
-    echo [3/3] Compilacion OK! Ejecutando...
-    start FastEditor.exe
+    exit /b 1
 )
+
+echo [3/3] Compilacion OK! Ejecutando...
+start FastEditor.exe
